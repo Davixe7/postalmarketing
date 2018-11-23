444f9d9f4ef12d195a46a4668512c109d86878eb
David Guilarte <davidguilarte7@gmail.com>
2018-11-23 06:29:30 -0430

    Setup collect model

--- app/Collect.php
+++ app/Collect.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Collect extends Model
 {
-    //
+    protected $fillable = ['workload_id','client_id','confirmed','status_id'];
+
+    public function workload(){
+      return $this->belongsTo('App\Workload');
+    }
+
+    public function client(){
+      return $this->belongsTo('App\Client');
+    }
 }
