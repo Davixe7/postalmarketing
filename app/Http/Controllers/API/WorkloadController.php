3de01cd46cd586b9c06c2caad4b6bc174b745976
David Guilarte <davidguilarte7@gmail.com>
2018-11-23 06:23:20 -0430

    Created all modules for Collect

--- app/Http/Controllers/API/WorkloadController.php
+++ app/Http/Controllers/API/WorkloadController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Workload;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -14,7 +15,7 @@ class MigrationController extends Controller
 
     public function store(Request $request)
     {
-      
+
     }
 
     public function show($id)
